70237ab94dc38d64c2f9d4f74380246c7d3a8de1
AmeyaKulkarni2001 <71250891+AmeyaKulkarni2001@users.noreply.github.com>
2021-04-08 14:04:02 +0530

    Update Sort in ascending order.asm

--- Sort in ascending order.asm
+++ Sort in ascending order.asm
@@ -1,20 +1,72 @@
 section .data
-    num_Array = 5,12,0D3,8,0A
-    msg db "Array in ascending order is: ", 10
-    msglen equ $-msg
+msg db "sorted array is: ", 10
+msglen equ $-msg
+arr db 08h,0Bh,80h,0D2h,15h
+
+%macro op 4
+mov rax,%1
+mov rdi,%2
+mov rsi,%3
+mov rdx,%4
+syscall
+%endmacro
+
+section .bss
+res resb 15
 
 section .text
     global _start
-
 _start:
-    loop1: 
-    mov rsi,num_Array   ;; rsi = 5
-    mov bl,rsi          ;; bl = 5
-    cmp bl,[rsi + 1]    ;; bl = 5 rsi + 1 = 12
-    jbe loop1           ;; cmp 5 and 12
+
+mov bl,5
+outer_loop1:
+            mov cl,4
+            mov rsi,arr
+up1:
+            mov al,byte[rsi]
+            cmp al,byte[rsi + 1]
+            jbe inc_num1
     
-    mov dl,[rsi]
-    dec rsi
-    mov [rsi],dl
-    inc rsi
-    jmp loop1
+            xchg al,byte[rsi + 1]
+            mov byte[rsi],al
+inc_num1:
+            inc rsi
+            dec cl
+            jnz up1
+        
+            dec bl
+            jnz outer_loop1
+            
+            op 1,1,msg,msglen
+            
+            mov rdi, arr
+            mov rsi, res
+            mov al,[rdi]
+            
+disp_loop1:
+	        mov cl,2
+	        mov al,[rdi]
+            
+againx1:
+            rol al,4
+            mov bl,al
+            and al,0FH
+            cmp al,09H
+            jbe downx1
+            add al,07H
+downx1:
+            add al,30H
+            mov byte[rsi],al
+            mov al,bl
+            inc rsi
+            dec cl
+            jnz againx1
+            
+            mov byte[rsi],0AH
+			inc rsi			
+			inc rdi
+			dec dl
+			jnz disp_loop1
+			op 1,1,res,15
+			
+			op 60,0,0,0
